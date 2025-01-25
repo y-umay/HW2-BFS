8041dd8888fbe61373ca3aa90a30ccaae23f0fc3
Yagmur Umary Doruk <yagmur.dorukpolcyn@ucsf.edu>
2025-01-24 22:20:12 -0800

    tests passing

--- search/graph.py
+++ search/graph.py
@@ -8,9 +8,13 @@ class Graph:
     """
     def __init__(self, filename: str):
         """
-        Initialization of graph object 
+        Initialization of graph object.
         """
-        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        # Raises error if the file does not exist.
+        try:
+            self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File '{filename} not found.'")
 
     def bfs(self, start, end=None):
         """
@@ -20,9 +24,25 @@ class Graph:
         * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
         * If there is an end node input and a path does not exist, return None
 
+        Perform BFS traversal or find the shortest path.
+        Args:
+            start (str): The starting node.
+            end (str, optional): The destination node.
+        Returns:
+            List[str]: BFS traversal order or the shortest path.
+            None: If no path exists to the destination node.
+        Raises:
+            KeyError: If the start and end node does not exist in the graph.
+            ValueError: If the graph is empty.
         """
+        if len(self.graph) == 0:
+            raise ValueError(f"'{filename} does not contain a graph.'")
+        if start not in self.graph:
+            raise KeyError(f"Start node '{start}' not found in the graph")
         if end == None:
             return self._bfs_traversal(start)
+        if end not in self.graph:
+            raise KeyError(f"End node '{end}' not found in the graph")
         return self._bfs_shortest_path(start, end)
     
     def _bfs_traversal(self, start):
@@ -40,25 +60,25 @@ class Graph:
         return visited
     
     def _bfs_shortest_path(self, start, end):
-        parents = {}
+        shortest_path_parent = {}
         queue = []
         queue.append(start)
-        parents[start] = None
+        shortest_path_parent[start] = None
         while len(queue) != 0:
             current_vertext = queue.pop(0)
             neighbors = self.graph.successors(current_vertext)
             for neighbor in neighbors:
-                if neighbor not in parents:
-                    parents[neighbor] = current_vertext
+                if neighbor not in shortest_path_parent:
+                    shortest_path_parent[neighbor] = current_vertext
                     queue.append(neighbor)
         # check if path exists
-        if parents.get(end) is None:
+        if shortest_path_parent.get(end) is None:
             return None
         # now gather shortest path from start->end in reverse order
         shortest_path_reverse = [end]
         current_vertext = end
-        while parents[current_vertext] != None:
-            current_vertext = parents[current_vertext]
+        while shortest_path_parent[current_vertext] != None:
+            current_vertext = shortest_path_parent[current_vertext]
             shortest_path_reverse.append(current_vertext)
         # now reverse child->parent ordering, make it parent->child
         shortest_path = []
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,7 +1,28 @@
 # write tests for bfs
 import pytest
-from search import graph
+import search
+import networkx as nx
 
+
+def verify_bfs_order(graph_file, start_node):
+    """
+    Generates a traversal order as a comparision point 
+    for BFS implementation validation
+    """
+    graph = nx.read_adjlist(graph_file, create_using=nx.DiGraph, delimiter=";")
+    bfs_order = list(nx.bfs_edges(graph, start_node))
+    nodes_in_order = [start_node] + [edge[1] for edge in bfs_order]
+    return nodes_in_order
+
+def verify_shortest_path(file, start, end):
+    """
+    Generates the shortest path as a comparision point 
+    for BFS implementation validation
+    """
+    graph = nx.read_adjlist(file, create_using=nx.DiGraph, delimiter=";")
+    shortest_expected_path = nx.shortest_path(graph, source=start, target=end)
+    return shortest_expected_path
+    
 def test_bfs_traversal():
     """
     TODO: Write your unit test for a breadth-first
@@ -10,7 +31,17 @@ def test_bfs_traversal():
     that all nodes are being traversed (ie. returns 
     the right number of nodes, in the right order, etc.)
     """
-    pass
+    graph = search.Graph("data/tiny_network.adjlist")
+    # Generate traversal using BFS implementation
+    traversal = graph.bfs("Luke Gilbert")
+    len_traversal = len(traversal)
+    # Generate expected order using networkx
+    expected_traversal = verify_bfs_order("data/tiny_network.adjlist", "Luke Gilbert")
+    len_expected_traversal = len(expected_traversal)
+    # Assert both orders match
+    assert traversal == expected_traversal, "The order of nodes is not right."
+    # Assert both number of nodes match
+    assert len_traversal == len_expected_traversal, "The number of nodes is not right."
 
 def test_bfs():
     """
@@ -23,4 +54,21 @@ def test_bfs():
     Include an additional test for nodes that are not connected 
     which should return None. 
     """
-    pass
+    # Get the shortest path using BFS implementation
+    graph = search.Graph("data/citation_network.adjlist")
+    shortest_path = graph.bfs("Luke Gilbert", "Hani Goodarzi")
+    # Generate the shortest expected path using networkx
+    expected_shortest_path = verify_shortest_path("data/citation_network.adjlist", "Luke Gilbert", "Hani Goodarzi")
+    # Assert that the returned shortest path matches the expected shortest path
+    assert shortest_path == expected_shortest_path, f"Expected {expected_shortest_path}, but got {shortest_path}."
+    #Test disconnected nodes
+    no_path = graph.bfs("34803213", "Brian Shoichet")
+    assert no_path is None, "Nonexistent node is found."
+    #Test non-existent start node
+    with pytest.raises(KeyError):
+        graph.bfs("NonExistentStartNode", "Luke Gilbert")
+    #Test non-existent end node
+    with pytest.raises(KeyError):
+        graph.bfs("Luke Gilbert", "NonExistentEndNode")
+
+    
